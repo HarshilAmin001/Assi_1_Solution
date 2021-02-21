8f82f93512f0b416c359e73ee12d1c21a91f6dc4
HarshilAmin001 <79329690+HarshilAmin001@users.noreply.github.com>
2021-02-21 06:12:07 -0500

    Rectangle CLass and Rectangle Test Class and Nunit Class Generated

--- Assi_1_Solution/Program.cs
+++ Assi_1_Solution/Program.cs
@@ -1,12 +1,1 @@
-﻿using System;
-
-namespace Assi_1_Solution
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+﻿
